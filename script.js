989c5d3fe889d44e4518f562ae88dc1d8f81006b
Raiseli <Raiseli@Raiselis-MacBook-Pro.local>
2019-02-04 20:15:28 +0100

    edit fn

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-let imgOne, imgTwo, imgThree, imgFour, imgFive, imgSix;
+tdGrid();
 
-imgOne      = "<img src='images/facebook.png' class='center'>";
-imgTwo      = "<img src='images/twitter.png' class='center'>";
-imgThree    = "<img src='images/snapchat.png' class='center'>";
-imgFour     = "<img src='images/linkedin.png' class='center'>";
-imgFive     = "<img src='images/youtube.png' class='center'>";
-imgSix      = "<img src='images/instagram.png' class='center'>";
+function tdGrid() {
+    let grid = document.getElementsByTagName("td");
 
-tdOne.addEventListener('click', () => {
-    tdOne.innerHTML = imgOne;
-    tdOne.style.background = "white";
-});
+    for(let i = 0; i < grid.length; i++) {
+        let cell = grid[i];
 
-tdTwo.addEventListener('click', () => {
-    tdTwo.innerHTML = imgTwo;
-    tdTwo.style.background = "white";
-});
+        cell.addEventListener('click', () => {     
+            cell.innerHTML = randomImages();     
+            cell.style.background = "white"; 
+        
+            console.log('Clicked');
+        });
+    }
+}
 
-tdThree.addEventListener('click', () => {
-    tdThree.innerHTML = imgThree;
-    tdThree.style.background = "white";
-});
+function randomImages() {
+    let imgArr = [
+        "<img src='images/facebook.png' class='center'>", 
+        "<img src='images/facebook.png' class='center'>",
+        "<img src='images/twitter.png' class='center'>",
+        "<img src='images/twitter.png' class='center'>",
+        "<img src='images/snapchat.png' class='center'>",
+        "<img src='images/snapchat.png' class='center'>",
+        "<img src='images/linkedin.png' class='center'>",
+        "<img src='images/linkedin.png' class='center'>",
+        "<img src='images/youtube.png' class='center'>",
+        "<img src='images/youtube.png' class='center'>",
+        "<img src='images/instagram.png' class='center'>",
+        "<img src='images/instagram.png' class='center'>"];
 
-tdFour.addEventListener('click', () => {
-    tdFour.innerHTML = imgFour;
-    tdFour.style.background = "white";
-});
-
-tdFive.addEventListener('click', () => {
-    tdFive.innerHTML = imgFive;
-    tdFive.style.background = "white";
-});
-
-tdSix.addEventListener('click', () => {
-    tdSix.innerHTML = imgSix;
-    tdSix.style.background = "white";
-});
+    let random  = imgArr[Math.floor(Math.random() * (imgArr.length - 1))];
+    return random;
+}
