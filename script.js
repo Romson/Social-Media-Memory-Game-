f4206227b92e822b22052f88a3625e00ac0240be
Raiseli <Raiseli@Raiselis-MacBook-Pro.local>
2019-02-06 23:29:17 +0100

    edit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+// Memory game
+
+let pairs_array = [];
+let gameCompleted = 0;
+
 tdGrid();
 
+// Id td Grid add to cell
 function tdGrid() {
     let grid = document.getElementsByTagName("td");
 
@@ -7,15 +13,27 @@ function tdGrid() {
         let cell = grid[i];
 
         cell.addEventListener('click', () => {     
-            cell.innerHTML = randomImages();     
-            cell.style.background = "white"; 
-        
-            console.log('Clicked');
+            cell.innerHTML = randomImage();     
+            cell.style.background = "white";
+            pairs_array.push(cell.innerHTML);
+
+            // Change bgColor if pair found
+            if (pairs_array.length == 2) {
+                if(pairs_array[0] != pairs_array[1]) {
+                    cell.style.backgroundColor = "red"; 
+                    cell.innerHTML = "";
+                }
+                else if(pairs_array[0] == pairs_array[1]) {
+                    cell.style.backgroundColor = "purple";
+                    pairs_array = [];
+                }
+            } 
         });
     }
 }
 
-function randomImages() {
+// Random image
+function randomImage() {
     let imgArr = [
         "<img src='images/facebook.png' class='center'>", 
         "<img src='images/facebook.png' class='center'>",
